3e26973ab70450dbf8560622ebb24e8518237421
Cont16 <martinalexandercont16@gmail.com>
2025-08-06 20:08:54 -0600

    nuevo cont

--- models/FormularioTicket.php
+++ models/FormularioTicket.php
@@ -4,14 +4,6 @@ namespace Model;
 
 class FormulariTicket extends ActiveRecord {
     public static $tabla = 'formulario_ticket';
-    public static $columnasDB = [
-        'form_tick_num',
-        'form_tic_usu',
-        'tic_dependencia',
-        'tic_comentario_falla',
-        'tic_correo_electronico',
-        'form_ticket_num',
-        
 
-    ];
+
 }

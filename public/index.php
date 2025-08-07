bff88701f339a2199325a3ac4b6f4bea0f22936b
Juarezgump <josesotecast138@gmail.com>
2025-08-07 16:56:43 -0600

    Prueba de formulario

--- public/index.php
+++ public/index.php
@@ -9,5 +9,7 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [AppController::class,'index']);
 
+$router->get('/crear', [TicketController::class,'renderizarPagina']);
+
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
